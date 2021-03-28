39b8c1eacee5fdc0fb28ce6d5febc7470905c070
Ataing1 <56658290+Ataing1@users.noreply.github.com>
2021-03-27 22:04:09 -0700

    partial server implemenation

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const { resolve } = require('path');
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
 
 if (process.env.NODE_ENV === 'development') {
 	console.log("THIS IS DEVELOPMENT MODE");
@@ -16,6 +19,27 @@ app.get('/', (req, res) => {
 	const path = resolve(process.env.STATIC_DIR + '/index.html');
 	res.sendFile(path);
 });
+
+app.get('/new-room', (req, res) => {
+	res.redirect(`/${uuidV4()}`)
+})
+
+
+app.get('/:room', (req, res) => {
+	res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+	socket.on('join-room', (roomId, userId) => {
+		socket.join(roomId)
+		socket.to(roomId).broadcast.emit('user-connected', userId)
+
+		socket.on('disconnect', () => {
+			socket.to(roomId).broadcast.emit('user-disconnected', userId)
+		})
+	})
+})
+
 
 let port = process.env.PORT || 1111;
 if (port === 1111) {
